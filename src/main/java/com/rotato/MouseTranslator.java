7dc53a863b8b1df3d710b196fd414c8b17613733
SteveM <meigss@my.easternct.edu>
2017-08-05 01:05:10 -0400

    seperate MouseTranslator class

---
+++ src/main/java/com/rotato/MouseTranslator.java
@@ -0,0 +1,35 @@
+package com.rotato;
+
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Robot;
+
+public class MouseTranslator {
+	private Robot robot;
+	
+	public MouseTranslator() {
+	}
+	
+	public void setRobot() throws AWTException {
+		robot = new Robot();
+	}
+	
+	public Point currentPos() {
+		java.awt.Point pointer = MouseInfo.getPointerInfo().getLocation();
+		int x = (int) pointer.getX();
+		int y = (int) pointer.getY();
+		
+		return new Point(x, y);
+	}
+	
+	public void translate(int x, int y, int steps, int sleep) throws InterruptedException {
+		for (int z = 0; z < steps; z++) {
+			Point curPos = currentPos();
+			int xPos = curPos.getX();
+			int yPos = curPos.getY();
+			
+			robot.mouseMove(xPos + x, yPos + y);
+			Thread.sleep(sleep);
+		}
+	}
+}

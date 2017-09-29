71c6b88c8581de5b91af250f2a5d937a8b019ba4
Thanoj Singh <thanoj.singh@gmail.com>
2017-09-29 01:38:05 -0400

    figure out glass pane

--- src/main/java/com/rotato/gui/view/HotkeyView.java
+++ src/main/java/com/rotato/gui/view/HotkeyView.java
@@ -15,11 +15,14 @@ public class HotkeyView extends JPanel {
 	 * Create the panel.
 	 */
 	public HotkeyView() {
+		GlassPane gp = new GlassPane(); // This is for testing remove this later
+		add(gp);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 100, 100, 0 };
-		gridBagLayout.rowHeights = new int[] { 50, 50, 50, 0 };
-		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.columnWidths = new int[]{100, 100, 0};
+		gridBagLayout.rowHeights = new int[]{50, 50, 50, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0,
+				Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 
 		JButton btnMoveLeft = new JButton("Move Left");
@@ -66,6 +69,8 @@ public class HotkeyView extends JPanel {
 		btnNotImplemented.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				System.out.println("here");
+				gp.setVisible(true);
 			}
 		});
 		GridBagConstraints gbc_btnNotImplemented = new GridBagConstraints();

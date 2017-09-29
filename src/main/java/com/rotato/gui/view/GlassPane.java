bb32c7feff2c76377a546975fdd11aa92161bf8e
Thanoj Singh <thanoj.singh@gmail.com>
2017-09-29 17:58:38 -0400

    use layer panel for hotkeys

--- src/main/java/com/rotato/gui/view/GlassPane.java
+++ src/main/java/com/rotato/gui/view/GlassPane.java
@@ -1,22 +1,13 @@
 package com.rotato.gui.view;
 
-import java.awt.SystemColor;
+import java.awt.Color;
+import java.awt.Dimension;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 public class GlassPane extends JPanel {
-
 	public GlassPane() {
-		setBackground(SystemColor.info);
-	}
-
-	@Override
-	public void setVisible(boolean on) {
-		JFrame frame = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class,
-				this);
-		frame.setGlassPane(this);
-		super.setVisible(on);
+		setBackground(new Color(255, 228, 181));
+		this.setSize(new Dimension(300, 300));
 	}
 }

821815a2757f3d45b6ab9a971c5b2b11a7164906
Thanoj Singh <thanoj.singh@gmail.com>
2018-01-07 15:41:17 -0500

    clipboard text works better with multiple clicks

--- src/main/java/com/rotato/gui/view/CounterView.java
+++ src/main/java/com/rotato/gui/view/CounterView.java
@@ -7,11 +7,14 @@ import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 
@@ -22,22 +25,24 @@ public class CounterView extends JPanel {
 	public CounterView() {
 		setBorder(new EmptyBorder(25, 10, 25, 10));
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{100};
-		gridBagLayout.rowHeights = new int[]{50, 50};
-		gridBagLayout.columnWeights = new double[]{0.0};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0};
+		gridBagLayout.columnWidths = new int[] { 100 };
+		gridBagLayout.rowHeights = new int[] { 50, 50 };
+		gridBagLayout.columnWeights = new double[] { 0.0 };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0 };
 		setLayout(gridBagLayout);
 
 		JPanel down = new JPanel();
+		down.setBorder(new TitledBorder(new EmptyBorder(0, 0, 0, 0), " ", TitledBorder.CENTER, TitledBorder.TOP, null,
+				new Color(0, 102, 0)));
 		GridBagConstraints gbc_down = new GridBagConstraints();
 		gbc_down.gridx = 0;
 		gbc_down.gridy = 1;
 		add(down, gbc_down);
 		GridBagLayout gbl_down = new GridBagLayout();
-		gbl_down.columnWidths = new int[]{100, 100};
-		gbl_down.rowHeights = new int[]{50};
-		gbl_down.columnWeights = new double[]{0.0, 0.0};
-		gbl_down.rowWeights = new double[]{0.0};
+		gbl_down.columnWidths = new int[] { 100, 100 };
+		gbl_down.rowHeights = new int[] { 50 };
+		gbl_down.columnWeights = new double[] { 0.0, 0.0 };
+		gbl_down.rowWeights = new double[] { 0.0 };
 		down.setLayout(gbl_down);
 
 		JLabel lblDown = new JLabel("Down");
@@ -55,18 +60,17 @@ public class CounterView extends JPanel {
 		lblDownCounter.setFont(new Font("Tahoma", Font.PLAIN, 16));
 
 		JPanel left = new JPanel();
-		left.setBorder(new TitledBorder(null, "Copied to Clipboard",
-				TitledBorder.CENTER, TitledBorder.TOP, null,
-				new Color(0, 255, 0)));
+		left.setBorder(new TitledBorder(new EmptyBorder(0, 0, 0, 0), " ", TitledBorder.CENTER, TitledBorder.TOP, null,
+				new Color(0, 102, 0)));
 		GridBagConstraints gbc_left = new GridBagConstraints();
 		gbc_left.gridx = 0;
 		gbc_left.gridy = 0;
 		add(left, gbc_left);
 		GridBagLayout gbl_left = new GridBagLayout();
-		gbl_left.columnWidths = new int[]{100, 100};
-		gbl_left.rowHeights = new int[]{50};
-		gbl_left.columnWeights = new double[]{0.0, 0.0};
-		gbl_left.rowWeights = new double[]{0.0};
+		gbl_left.columnWidths = new int[] { 100, 100 };
+		gbl_left.rowHeights = new int[] { 50 };
+		gbl_left.columnWeights = new double[] { 0.0, 0.0 };
+		gbl_left.rowWeights = new double[] { 0.0 };
 		left.setLayout(gbl_left);
 
 		JLabel lblLeft = new JLabel("Left");
@@ -83,8 +87,8 @@ public class CounterView extends JPanel {
 		left.add(lblLeftCounter, gbc_lblLeftCounter);
 		lblLeftCounter.setFont(new Font("Tahoma", Font.PLAIN, 16));
 
-		down.addMouseListener(new ClipboardCopy(lblDownCounter));
-		left.addMouseListener(new ClipboardCopy(lblLeftCounter));
+		down.addMouseListener(new ClipboardCopy(lblDownCounter, down));
+		left.addMouseListener(new ClipboardCopy(lblLeftCounter, left));
 	}
 
 	public void setCounters(int left, int down) {
@@ -95,14 +99,43 @@ public class CounterView extends JPanel {
 
 class ClipboardCopy extends MouseAdapter {
 	private JLabel label;
-	public ClipboardCopy(JLabel label) {
+	private JPanel panel;
+	private TitledBorder border;
+	private Timer timer;
+
+	public ClipboardCopy(JLabel label, JPanel panel) {
 		this.label = label;
+		this.panel = panel;
+		this.border = (TitledBorder) panel.getBorder();
+		this.timer = new Timer(3000, clearTitle());
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		TitledBorder border = (TitledBorder) panel.getBorder();
+		String originalText = border.getTitle();
+
 		StringSelection stringSelection = new StringSelection(label.getText());
 		Clipboard clpboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 		clpboard.setContents(stringSelection, null);
+
+		border.setTitle("Copied to Clipboard");
+		panel.setBorder(border);
+		panel.repaint();
+
+		timer.restart();
+	}
+
+	public ActionListener clearTitle() {
+		ActionListener taskPerformer = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				border.setTitle(" ");
+				panel.setBorder(border);
+				panel.repaint();
+			}
+		};
+
+		return taskPerformer;
 	}
 }
